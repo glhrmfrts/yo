5fb0547f114b2369dcbca17c35945a9bfe628368
Guilherme Nemeth <guilherme.nemeth@gmail.com>
2016-02-15 13:46:18 -0300

    convert indentation to spaces

--- state.go
+++ state.go
@@ -4,42 +4,42 @@ import (
 )
 
 const (
-	MaxRegisters  = 249
-	CallStackSize = 255
+  MaxRegisters  = 249
+  CallStackSize = 255
 )
 
 type callFrame struct {
-	pc 		int
-	fn  	*Func
-	r 		[MaxRegisters]Value
+  pc    int
+  fn    *Func
+  r     [MaxRegisters]Value
 }
 
 type callFrameStack struct {
-	sp 		int
-	stack [CallStackSize]callFrame
+  sp    int
+  stack [CallStackSize]callFrame
 }
 
 type State struct {
-	currentFrame *callFrame
-	calls 			 callFrameStack
+  currentFrame *callFrame
+  calls        callFrameStack
 }
 
 
 func (stack *callFrameStack) New() *callFrame {
-	stack.sp += 1
-	return &stack.stack[stack.sp-1]
+  stack.sp += 1
+  return &stack.stack[stack.sp-1]
 }
 
 func (stack *callFrameStack) Back() *callFrame {
-	if stack.sp == 0 {
-		return nil
-	}
-	return &stack.stack[stack.sp-1]
+  if stack.sp == 0 {
+    return nil
+  }
+  return &stack.stack[stack.sp-1]
 }
 
 func (state *State) RunProto(proto *FuncProto) {
-	state.currentFrame = state.calls.New()
-	state.currentFrame.fn = &Func{proto}
+  state.currentFrame = state.calls.New()
+  state.currentFrame.fn = &Func{proto}
 
-	execute(state)
+  execute(state)
 }
